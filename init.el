b13400c05ceb67529f3366842b7ffb29e8c94193
tsuigeo <tsui.geoffrey@gmail.com>
2024-10-20 05:18:41 +0800

    use doom modeline

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
+   '("ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
  '(package-selected-packages '(projectile which-key swiper spacemacs-theme magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
