d8a35faa004ed7bed994a2308a2d2569fe600b12
tsuigeo <tsui.geoffrey@gmail.com>
2024-10-21 03:05:11 +0800

    add tree-sitter

--- init.el
+++ init.el
@@ -7,8 +7,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
- '(package-selected-packages '(projectile which-key swiper spacemacs-theme magit)))
+   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
+ '(package-selected-packages
+   '(company-box company tree-sitter-langs tree-sitter projectile which-key swiper spacemacs-theme magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
